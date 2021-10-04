d317273160d5e45b4066effd0bc87491d23e5749
Dzmitry Kishylau <dzmitry_kishylau@Dzmitrys-MacBook-Pro.local>
2021-10-04 11:15:45 -0700

    Show empty panel

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,42 @@ import {
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
+import { ICommandPalette, MainAreaWidget } from '@jupyterlab/apputils';
+import { Widget } from '@lumino/widgets';
+
 /**
  * Initialization data for the sideexplore extension.
  */
 const plugin: JupyterFrontEndPlugin<void> = {
   id: 'sideexplore:plugin',
   autoStart: true,
-  activate: (app: JupyterFrontEnd) => {
+  activate: (app: JupyterFrontEnd, palette: ICommandPalette) => {
     console.log('JupyterLab extension sideexplore is activated!');
-  }
+
+    // Create blank content widget inside of MainAreaWidget
+    const content = new Widget();
+    const widget = new MainAreaWidget({ content });
+    widget.id = 'sideexplore-jupyterlab';
+    widget.title.label = 'Astronomy Picture';
+    widget.title.closable = true;
+
+    // Add an application command
+    const command: string = 'sideexplore:open';
+    app.commands.addCommand(command, {
+      label: 'Random Astronomy Picture',
+      execute: () => {
+        if (!widget.isAttached) {
+          // Attach the widget to the main work area if it's not there
+          app.shell.add(widget, 'main');
+        }
+        // Activate the widget
+        app.shell.activateById(widget.id);
+      }
+    })
+
+    palette.addItem({ command, category: 'Tutorial' });
+  },
+  requires: [ICommandPalette]
 };
 
 export default plugin;
